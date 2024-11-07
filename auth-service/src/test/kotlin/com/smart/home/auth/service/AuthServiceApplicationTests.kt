44e59b46d824b6e4b2d110631ea08c1cf3ca82f3
Elizabeth Doonily <liza.ry-samara@mail.ru>
2024-11-07 21:13:27 +0400

    init auth and user micros

---
+++ auth-service/src/test/kotlin/com/smart/home/auth/service/AuthServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.smart.home.auth.service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
